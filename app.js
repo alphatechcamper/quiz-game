7a00a9429a2a4895f457b3f4409901a6dc25b932
alphatechcamper <mizumoto2017@gmail.com>
2023-08-18 14:51:30 +0900

    クイズゲーム（応用）

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
-const question = 'ゲーム史上、最も売れたゲーム機は次のうちどれ？';
-const answers = [
-  'スーパーファミコン',
-  'プレイステーション',
-  'ニンテンドーDS',
-  'ニンテンドースイッチ'
+const quiz = [
+  {
+    question: 'ゲーム史上、最も売れたゲーム機はどれ？',
+    answers: [ 'スーパーファミコン', 'PlayStation 2', 'ニンテンドーDS', 'Xbox 360'],
+    correct: 'ニンテンドーDS'
+  }, {
+    question: '糸井重里が企画に関わった、任天堂の看板ゲームといえば？',
+    answers: [ 'MOTHER2', 'スーパーマリオブラザーズ3', 'スーパードンキーコング', '星のカービィ'],
+    correct: 'MOTHER2'
+  }, {
+    question: 'ファイナルファンタジーⅣの主人公の名前は？',
+    answers: [ 'フリオニール', 'クラウド', 'ティーダ', 'セシル'],
+    correct: 'セシル'
+  }
 ];
-const correct = 'ニンテンドーDS';
+
+const quizLength = quiz.length;
+let quizIndex = 0;
+
 const $button = document.getElementsByTagName('button');
 const buttonLength = $button.length;
 
 // クイズの問題分、選択肢を定義
 const setupQuiz = () => {
-  document.getElementById('js-question').textContent = question ;
+  document.getElementById('js-question').textContent = quiz[quizIndex].question ;
   let buttonIndex = 0;
   while(buttonIndex < buttonLength){
-    $button[buttonIndex].textContent = answers[buttonIndex];
+    $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
     buttonIndex++;
   }
 }
@@ -22,11 +33,21 @@ const setupQuiz = () => {
 setupQuiz();
 
 const clickHandler = (e) => {
-    if(correct === e.target.textContent) { //target:クリックされたボタン
+    if(quiz[quizIndex].correct === e.target.textContent) { //target:クリックされたボタン
       window.alert('正解！');
     } else {
       window.alert('不正解！');
     }
+
+    quizIndex++;
+
+    if(quizIndex < quizLength){
+      setupQuiz();
+    } else {
+      //問題数がもうなければこれを実行
+      window.alert('終了');
+    }
+
 };
 
 // ボタンをクリックしたら正誤判定
