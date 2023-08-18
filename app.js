84a5f097a6427f9be2bb8e5d66e36fe2162516c0
alphatechcamper <mizumoto2017@gmail.com>
2023-08-18 14:33:04 +0900

    リファクタリング

--- app.js
+++ app.js
@@ -6,44 +6,34 @@ const answers = [
   'ニンテンドースイッチ'
 ];
 const correct = 'ニンテンドーDS';
-
-// 定数の文字列をHTMLに反映させる
-document.getElementById('js-question').textContent = question ;
-
-// $:HTMLのオブジェクトが入っていることとしての印
 const $button = document.getElementsByTagName('button');
+const buttonLength = $button.length;
 
-$button[0].textContent = answers[0];
-$button[1].textContent = answers[1];
-$button[2].textContent = answers[2];
-$button[3].textContent = answers[3];
+// クイズの問題分、選択肢を定義
+const setupQuiz = () => {
+  document.getElementById('js-question').textContent = question ;
+  let buttonIndex = 0;
+  while(buttonIndex < buttonLength){
+    $button[buttonIndex].textContent = answers[buttonIndex];
+    buttonIndex++;
+  }
+}
+
+setupQuiz();
+
+const clickHandler = (e) => {
+    if(correct === e.target.textContent) { //target:クリックされたボタン
+      window.alert('正解！');
+    } else {
+      window.alert('不正解！');
+    }
+};
 
 // ボタンをクリックしたら正誤判定
-$button[0].addEventListener('click', () => {
-  if(correct === $button[0].textContent) {
-    window.alert('正解！');
-  } else {
-    window.alert('不正解！');
-  }
-});
-$button[1].addEventListener('click', () => {
-  if(correct === $button[1].textContent) {
-    window.alert('正解！');
-  } else {
-    window.alert('不正解！');
-  }
-});
-$button[2].addEventListener('click', () => {
-  if(correct === $button[2].textContent) {
-    window.alert('正解！');
-  } else {
-    window.alert('不正解！');
-  }
-});
-$button[3].addEventListener('click', () => {
-  if(correct === $button[3].textContent) {
-    window.alert('正解！');
-  } else {
-    window.alert('不正解！');
-  }
-});
+let handerIndex = 0;
+while (handerIndex < buttonLength) {
+  $button[handerIndex].addEventListener('click', (e) => {
+    clickHandler(e);
+  });
+  handerIndex++;
+}
